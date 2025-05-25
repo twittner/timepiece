ea1ed781ebfa7d650d31eae89fc8280fb47c3ca1
Toralf Wittner <tw@dtex.org>
2025-05-25 23:03:13 +0200

    Simplify.
    Signed-off-by: Toralf Wittner <tw@dtex.org>

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,28 @@
-use std::io;
+use std::io::{self, Write};
 use std::time::Duration;
 use std::thread::sleep;
 
 use anyhow::Result;
 use crossterm::{QueueableCommand, cursor, terminal::{Clear, ClearType}};
-use jiff::{Unit, Zoned, civil::TimeRound};
+use jiff::{Unit, Zoned};
 
 const FREQ: Duration = Duration::from_millis(1000);
 
 fn main() -> Result<()> {
-    let round = TimeRound::from((Unit::Millisecond, 100));
-
     let mut stdout = io::stdout();
 
-    clear_all(&mut stdout)?;
-
-    let now = Zoned::now().round(Unit::Second)?;
-    println!("{:?} {} {}", now.weekday(), now.date(), now.time().round(round)?);
-
-    loop {
-        let now = Zoned::now().round(Unit::Millisecond)?.time();
-        if now.millisecond() == 0 {
-            break
-        }
-        sleep(Duration::from_millis(1))
-    }
-
-    clear_line(&mut stdout)?;
+    stdout.queue(cursor::MoveTo(0, 0))?
+        .queue(cursor::Hide)?
+        .queue(Clear(ClearType::All))?
+        .flush()?;
 
     loop {
         let now = Zoned::now().round(Unit::Millisecond)?;
-        println!("{:?} {} {}", now.weekday(), now.date(), now.time().round(round)?);
+        println!("{:?} {} {}", now.weekday(), now.date(), now.time().round(Unit::Second)?);
         let delta = Duration::from_millis(now.time().millisecond() as u64);
         sleep(FREQ.saturating_sub(delta));
-        clear_line(&mut stdout)?;
+        stdout.queue(cursor::MoveToPreviousLine(1))?
+            .queue(Clear(ClearType::CurrentLine))?
+            .flush()?
     }
 }
-
-fn clear_all<W: io::Write>(w: &mut W) -> io::Result<()> {
-    w.queue(cursor::MoveTo(0, 0))?
-     .queue(cursor::Hide)?
-     .queue(Clear(ClearType::All))?
-     .flush()
-}
-
-fn clear_line<W: io::Write>(w: &mut W) -> io::Result<()> {
-    w.queue(cursor::MoveToPreviousLine(1))?
-     .queue(Clear(ClearType::CurrentLine))?
-     .flush()
-}
-
